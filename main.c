473fb2ff6b1a8681a8e2f284d3bf9b93c25a0c2a
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-05-19 08:40:22 +0300

    connect and query insturment

--- main.c
+++ main.c
@@ -2,14 +2,20 @@
 
 #include <windows.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "visa.h"
+
+static char stringinput[512];
+static unsigned char buffer[100];
+static ViUInt32 retCount;
+static ViUInt32 writeCount;
 
 int main()
 {
   printf("Step: 0\n");
   ViStatus status; // Error checking
-  ViSession defaultRM;
+  ViSession defaultRM, instr;
   ViUInt32 numInstrs; // Return count from string I/O
   ViFindList fList;
   ViChar desc[VI_FIND_BUFLEN];
@@ -26,18 +32,87 @@ int main()
     printf("VISA initialized.\n");
 
   status = viFindRsrc(defaultRM, "?*", &fList, &numInstrs, desc);
-  if (status < VI_SUCCESS)
-  {
-    printf("Can't initialize find\n");
-    return -1;
-  }
+  // if (status < VI_SUCCESS)
+  // {
+  //   printf("Can't initialize find\n");
+  //   return -1;
+  // }
+  if (status == VI_ERROR_INV_OBJECT)
+    printf("Invalid object\n");
+  else if (status == VI_ERROR_NSUP_OPER)
+    printf("Session does not support operation\n");
+  else if (status == VI_ERROR_INV_EXPR)
+    printf("Invalid expression\n");
+  else if (status == VI_ERROR_RSRC_NFOUND)
+    printf("Resource not found\n");
   else
   {
     printf("%s \n", desc);
-    for (int i = 0; i < numInstrs - 1; --i)
+    status = viOpen(defaultRM, desc, VI_NULL, VI_NULL, &instr);
+    if (status < VI_SUCCESS)
+      printf("An error occurred opening a session to %s\n", desc);
+    else
+    {
+      printf("Connected\n");
+      status = viSetAttribute(instr, VI_ATTR_TMO_VALUE, 5000);
+      strcpy(stringinput, "*IDN?");
+      status = viWrite(instr, (ViBuf)stringinput, (ViUInt32)strlen(stringinput), &writeCount);
+      if (status < VI_SUCCESS)
+      {
+        printf("Error writing to the device\n");
+      }
+
+      /*
+     * Now we will attempt to read back a response from the device to
+     * the identification query that was sent.  We will use the viRead
+     * function to acquire the data.  We will try to read back 100 bytes.
+     * After the data has been read the response is displayed.
+     */
+      status = viRead(instr, buffer, 100, &retCount);
+      if (status < VI_SUCCESS)
+      {
+        printf("Error reading a response from the device\n");
+      }
+      else
+      {
+        printf("Data read: %*s\n", retCount, buffer);
+      }
+      status = viClose(instr);
+    }
+    for (unsigned i = 0; i < numInstrs - 1; --i)
     {
       status = viFindNext(fList, desc); /* find next desriptor */
-      printf("%s \n", desc);
+      status = viOpen(defaultRM, desc, VI_NULL, VI_NULL, &instr);
+      if (status < VI_SUCCESS)
+        printf("An error occurred opening a session to %s\n", desc);
+      else
+      {
+        printf("Connected\n");
+        status = viSetAttribute(instr, VI_ATTR_TMO_VALUE, 5000);
+        strcpy(stringinput, "*IDN?");
+        status = viWrite(instr, (ViBuf)stringinput, (ViUInt32)strlen(stringinput), &writeCount);
+        if (status < VI_SUCCESS)
+        {
+          printf("Error writing to the device\n");
+        }
+
+        /*
+     * Now we will attempt to read back a response from the device to
+     * the identification query that was sent.  We will use the viRead
+     * function to acquire the data.  We will try to read back 100 bytes.
+     * After the data has been read the response is displayed.
+     */
+        status = viRead(instr, buffer, 100, &retCount);
+        if (status < VI_SUCCESS)
+        {
+          printf("Error reading a response from the device\n");
+        }
+        else
+        {
+          printf("Data read: %*s\n", retCount, buffer);
+        }
+        status = viClose(instr);
+      }
     }
     // {
     //   /* stay in this loop until we find all instruments */
@@ -61,6 +136,7 @@ int main()
   }
   printf("Found instruments: %d\n", numInstrs);
 
+  status = viClose(fList);
   status = viClose(defaultRM);
 
   return 0;
